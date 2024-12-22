c1a9d3dec49fbf801e9251572f90023297763c0b
Jan Matthew Vargas <157191677+matty-kun@users.noreply.github.com>
2024-12-22 22:24:55 +0800

    Added a frame for the Main Play Area

--- Simple_Tetris/src/main/Game_Panel.java
+++ Simple_Tetris/src/main/Game_Panel.java
@@ -2,13 +2,67 @@ package main;
 
 import javax.swing.JPanel;
 
-public class Game_Panel extends JPanel {
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+public class Game_Panel extends JPanel implements Runnable{
 	
 	// Screen Size
 	public static final int WIDTH = 1280;
 	public static final int HEIGHT = 720;
+	final int FPS = 60;
+	Thread gameThread;
+	PlayManager pm;
 	
 	public Game_Panel() {
 		
+		// Panel Settings
+		this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+		this.setBackground(Color.BLACK);
+		this.setLayout(null);
+		
+		pm = new PlayManager();
+	}
+	
+	public void launchGame() {
+		gameThread = new Thread(this);
+		gameThread.start();
+	}
+	
+	@Override
+	public void run() {
+		
+		// Game Loop
+		double drawInterval = 1000000000.0/FPS;
+		double delta = 0;
+		long lastTime = System.nanoTime();
+		long currentTime;
+		
+		while(gameThread != null) {
+			
+			currentTime = System.nanoTime();
+			
+			delta += (currentTime - lastTime) / drawInterval;
+			lastTime = currentTime;
+			
+			if(delta >= 1) {
+				update();
+				repaint();
+				delta--;
+			}
+		}
+	}
+	private void update() {
+		
+		pm.update();
+		
+	}
+	public void painComponent(Graphics g) {
+		super.paintComponent(g);
+		
+		Graphics2D g2 = (Graphics2D)g;
+		pm.draw(g2);
 	}
 }
--- Simple_Tetris/src/main/Main.java
+++ Simple_Tetris/src/main/Main.java
@@ -4,10 +4,18 @@ import javax.swing.JFrame;
 
 public class Main {
 	public static void main(String[] args) {
+		
+		// Make a Window
 		JFrame window = new JFrame();
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setResizable(false);
 
+		// Add Game_Panel to the Window
+		Game_Panel gp = new Game_Panel();
+		window.add(gp);
+		window.pack();
+		
+		
 		window.setLocationRelativeTo(null);
 		window.setVisible(true);
 		
