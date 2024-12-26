d41f84acc90a76e33a700968cadda614fdfdddb0
Jan Matthew Vargas <157191677+matty-kun@users.noreply.github.com>
2024-12-26 12:13:25 +0800

    Initial commit.

--- Simple_Tetris/src/mino/Mino_L2.java
+++ Simple_Tetris/src/mino/Mino_L2.java
@@ -5,10 +5,10 @@ import java.awt.Color;
 public class Mino_L2 extends Mino{
 	
 	public Mino_L2() {
-		create(Color.orange);
+		create(Color.blue);
 		
 	public void setXY(int x, int y) {
-		/* L1 looks like this:
+		/* L2 looks like this:
 			  #
 			  #
 			# #  
@@ -19,9 +19,29 @@ public class Mino_L2 extends Mino{
 		b[1].y = b[0].y - Block.SIZE;
 		b[2].x = b[0].x;
 		b[2].y = b[0].y + Block.SIZE;
-		b[3].x = b[0].x + Block.SIZE;
-		b[3].y = b[0].y - Block.SIZE;
+		b[3].x = b[0].x - Block.SIZE;
+		b[3].y = b[0].y + Block.SIZE;
 			
-		}
 	}
+	public void getDirection1() {
+		/* Direction1 (Default):
+			  #
+			  #
+			# #  
+		*/	
+	
+		tempb[0].x = b[0].x;
+		tempb[0].y = b[0].y;
+		tempb[1].x = b[0].x;
+		tempb[1].y = b[0].y - Block.SIZE;
+		tempb[2].x = b[0].x;
+		tempb[2].y = b[0].y + Block.SIZE;
+		tempb[3].x = b[0].x - Block.SIZE;
+		tempb[3].y = b[0].y + Block.SIZE;
+		
+		updateXY(1);
+	}
+	public void getDirection2() {}
+	public void getDirection3() {}
+	public void getDirection4() {}
 }
